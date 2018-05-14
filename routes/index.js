f48ae604cb6862aa455fb7ee9567069074ce50f2
Balaji29 <balajirk29@gmail.com>
2018-05-14 19:53:15 +0530

    Some small wrappers

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,22 @@ router.get('/', function(req, res, next) {
 
 // Webhooks that listens to the ApiAI.
 router.post('/', function(req, res, next) {
-  const parameters = req.body.queryResult.parameters;
-  console.log(parameters);
-  const payload = req.body.originalDetectIntentRequest.payload;
-  console.log('conversation ID', payload.conversation.conversationId);
+  //  const payload = req.body.originalDetectIntentRequest.payload;
+  //  Need to persist the conversation for the future reference.
+  //  console.log('conversation ID', payload.conversation.conversationId);
+  const params = req.body.queryResult.parameters;
+  const session = req.body.session;
+
+  console.log('session', session);
+  console.log('params', params);
+
+  // It's will be a switch case to filter the entities one by one.
 
   res.type('json');
-  res.json(fullFillment());
+  res.json(sendFinalMessage(params));
 });
 
-function fullFillment() {
+function sendFinalMessage(params) {
   return {
     "fulfillmentMessages": [
       {
@@ -25,14 +31,32 @@ function fullFillment() {
         "simpleResponses": {
           "simpleResponses": [
             {
-              "displayText": "This is working fine",
-              "ssml": "<speak>This is working fine. <break time=\"200ms\"/></speak>"
+              "displayText": 'this is working fine',
+              "ssml": `<speak>Assigned <break time="40ms"/> ${params.task} to <break time="40ms"/>${params.user}</speak>`
             }
           ]
         }
       }
     ]
   }
+}
+
+function handleTaskEvents(session, params, payload = {}) {
+  // Add task to the redis key/value pair.
+  // The key will be the session.
+}
+
+function handleNoteEvents(session, params, payload = {}) {
+  // Add note to the redis key/value pair.
+  // The key will be the session.
+}
+
+function handleMeetingCreate(session, params, payload = {}) {
+  // Fetch the google calendar for this user and perform the business logic.
+}
+
+function handleMeetingComplete(session, params, payload = {}) {
+  // Get the tasks and notes and post it to confluence/dropbox.
 }
 
 module.exports = router;
